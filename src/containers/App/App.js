6fa6fe3a2a778c25b36cf4cc2a5b3d4692550922
astro913 <astro913@163.com>
2017-08-08 10:18:24 +0800

    add

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -2,29 +2,32 @@ import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 // import { IndexLink } from 'react-router';
 import Helmet from 'react-helmet';
-// import { isLoaded as isInfoLoaded, load as loadInfo } from 'redux/modules/info';
-// import { isLoaded as isAuthLoaded, load as loadAuth, logout } from 'redux/modules/auth';
-import { logout } from 'redux/modules/auth';
+import { isLoaded as isInfoLoaded, load as loadInfo } from 'redux/modules/info';
+import { isLoaded as isAuthLoaded, load as loadAuth, logout } from 'redux/modules/auth';
+// import { logout } from 'redux/modules/auth';
 import { Navigation, Footer } from 'components';
-// import { push } from 'react-router-redux';
+import { push } from 'react-router-redux';
 // import config from '../../config';
-// import { asyncConnect } from 'redux-async-connect';
+import { asyncConnect } from 'redux-async-connect';
 
-// @asyncConnect([{
-//   promise: ({store: {dispatch, getState}}) => {
-//     const promises = [];
-//
-//     if (!isInfoLoaded(getState())) {
-//       promises.push(dispatch(loadInfo()));
-//     }
-//     if (!isAuthLoaded(getState())) {
-//       promises.push(dispatch(loadAuth()));
-//     }
-//
-//     return Promise.all(promises);
-//   }
-// }])
+@asyncConnect([{
+  promise: ({store: {dispatch, getState}}) => {
+    const promises = [];
 
+    if (!isInfoLoaded(getState())) {
+      promises.push(dispatch(loadInfo()));
+    }
+    if (!isAuthLoaded(getState())) {
+      promises.push(dispatch(loadAuth()));
+    }
+
+    return Promise.all(promises);
+  }
+}])
+
+@connect(
+    state => ({user: state.auth.user, transition: state.routing.locationBeforeTransitions, language: state.auth.language, }),
+    {logout, pushState: push})
 class App extends Component {
   static propTypes = {
     children: PropTypes.object.isRequired,
@@ -33,21 +36,22 @@ class App extends Component {
     language: PropTypes.string,
 
     transition: PropTypes.object,
+    pushState: PropTypes.func.isRequired
   };
 
-  // static contextTypes = {
-  //   store: PropTypes.object.isRequired
-  // };
+  static contextTypes = {
+    store: PropTypes.object.isRequired
+  };
 
-  // componentWillReceiveProps(nextProps) {
-  //   if (!this.props.user && nextProps.user) {
-  //     // login
-  //     this.props.pushState('/loginSuccess');
-  //   } else if (this.props.user && !nextProps.user) {
-  //     // logout
-  //     this.props.pushState('/');
-  //   }
-  // }
+  componentWillReceiveProps(nextProps) {
+    if (!this.props.user && nextProps.user) {
+      // login
+      this.props.pushState('/loginSuccess');
+    } else if (this.props.user && !nextProps.user) {
+      // logout
+      this.props.pushState('/');
+    }
+  }
 
   handleLogout = (event) => {
     event.preventDefault();
@@ -72,20 +76,20 @@ class App extends Component {
   }
 }
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    logout: () => {
-      dispatch(logout());
-    },
-  };
-};
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     logout: () => {
+//       dispatch(logout());
+//     },
+//   };
+// };
+//
+// const mapStateToProps = (state) => ({
+//   user: state.auth.user,
+//   transition: state.routing.locationBeforeTransitions,
+//   language: state.auth.language,
+// });
 
-const mapStateToProps = (state) => ({
-  user: state.auth.user,
-  transition: state.routing.locationBeforeTransitions,
-  language: state.auth.language,
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
-
+// export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
 //  <Helmet {...config.app.head}/>
